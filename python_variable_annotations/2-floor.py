7155242302b4c4a637114d43c27a8e2eca00ccbe
hagguishel <hagguirzt@gmail.com>
2025-07-30 13:26:18 +0200

    exo 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor: defines a type-annotated floor function
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n"""
+    return math.floor(n)
