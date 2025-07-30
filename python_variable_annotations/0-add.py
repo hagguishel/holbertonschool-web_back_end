cb94cfa79672a334e32634c052b0b2713cb15a44
hagguishel <hagguirzt@gmail.com>
2025-07-30 13:06:02 +0200

    exo 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Module 0-add: defines a type-annotated add function
+"""
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b as a float"""
+    return a + b
