5887f89b1e89682f0dfbca34b0270b4c2f1b3e04
hagguishel <hagguirzt@gmail.com>
2025-07-30 13:32:47 +0200

    correction

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -6,5 +6,5 @@ Module 4-define_variables: defines annotated variables
 
 a: int = 1
 pi: float = 3.14
-i_understand_annotation: bool = True
-school: str = 'Holbertoon'
+i_understand_annotations: bool = True
+school: str = 'Holberton'
