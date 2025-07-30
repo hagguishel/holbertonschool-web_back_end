a7315a2f7718b7e650b1c361298eba8f7360085c
hagguishel <hagguirzt@gmail.com>
2025-07-30 13:15:20 +0200

    exo 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module 1-concat: defines a type-annotated concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings"""
+    return str1 + str2
