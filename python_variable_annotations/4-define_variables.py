653d18da49257adae6e1fcd3a46ea1e6e5651103
hagguishel <hagguirzt@gmail.com>
2025-07-30 13:31:28 +0200

    exo 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module 4-define_variables: defines annotated variables
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotation: bool = True
+school: str = 'Holbertoon'
