c317cc9074721ef7307501d21e46ee74acd6b21a
hagguishel <hagguirzt@gmail.com>
2025-07-30 13:28:33 +0200

    exo 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str: defines a type-annotated to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float n"""
+    return str(n)
