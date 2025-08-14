f3721f893b3a6d4c521296dffcad8c3730819744
hagguishel <hagguirzt@gmail.com>
2025-08-14 10:55:32 +0200

    doc

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
-    """Calcule les indices de début et de fin pour une pagination."""
+    """
+    function named index_range that takes two integer
+    arguments page and page_size
+    """
     start: int = (page - 1) * page_size
-    end: int = page * page_size
+    end: int = page_size * page
     return (start, end)
