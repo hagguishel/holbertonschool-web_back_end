75fd82312a1554d0ba155ec1ad2c80fce5cae550
hagguishel <hagguirzt@gmail.com>
2025-09-03 14:07:49 +0200

    task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split(/\r?\n/);
+      const student = lines.slice(1).filter(line => line.trim() !== '');
+      console.log(`Number of students: ${student.length}`);
+
+      const groups = {};
+      for (const line of student) {
+        const cols = line.split(',');
+        const firstname = cols[0].trim();
+        const field = cols[cols.length - 1].trim();
+        if (!groups[field]) groups[field] = [];
+        groups[field].push(firstname)
+
+      }
+      for (const [field, list] of Object.entries(groups)) {
+        console.log(
+          `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`
+        )
+      };
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents
