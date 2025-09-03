41ff8aee142a8fccbdf2b738008d83d8f472ae25
hagguishel <hagguirzt@gmail.com>
2025-09-03 11:45:48 +0200

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?");
-
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  console.log("Your name is: " + data.toString().trim());
+  const name = data;
+  process.stdout.write(`Your name is: ${name}`);
+  process.exit(0);
 });
 
-process.stdin.on('end', () => {
-  console.log("This important software is now closing");
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
