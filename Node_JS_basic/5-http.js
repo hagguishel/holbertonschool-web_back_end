5c924202787df818a412f95c8b84979d592429e4
hagguishel <hagguirzt@gmail.com>
2025-09-04 10:42:51 +0200

    test task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,17 +4,17 @@ const countStudents = require('./3-read_file_async');
 const databaseFile = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  if (req.url ==='/') {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
-  return;
-}
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+    return;
+  }
 
 if (req.url === '/students') {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
 
   if (!databaseFile) {
     res.end('Cannot load the database');
@@ -22,7 +22,7 @@ if (req.url === '/students') {
   }
 
   const originalLog = console.log;
-  const lines =  [];
+  const lines = [];
   console.log = (...args) => {
     lines.push(args.join(' '));
   };
