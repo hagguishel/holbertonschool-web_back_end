d68e8c83486ee9e36633cbb3c9f6bd34860c794c
hagguishel <hagguirzt@gmail.com>
2025-09-04 09:35:07 +0200

    correction eslink

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,7 +4,6 @@ const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-
 });
 
 app.listen(1245);
